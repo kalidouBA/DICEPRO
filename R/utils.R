ac7f13c1ea428d9b1e15d43d79a7a208adde3a87
kalidouBA <kalidousid@gmail.com>
2025-04-02 16:08:17 +0200

    fix action issuesconvert matrix

--- R/utils.R
+++ R/utils.R
@@ -7,12 +7,6 @@
 #'
 #' @return A data frame with the same content, row names, and column names as the input matrix.
 #'         If the input is not a matrix, it is returned unchanged.
-#'
-#' @examples
-#' mat <- matrix(1:9, nrow = 3, dimnames = list(c("A", "B", "C"), c("X", "Y", "Z")))
-#' df <- convert_matrix_to_df(mat)
-#' print(df)
-#'
 
 convert_matrix_to_df <- function(mat) {
   if (is.matrix(mat)) {
