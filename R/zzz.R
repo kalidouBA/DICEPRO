4556ebc30a59a7f5cd48ba2ff40cb028b7f743ff
borishejblum <boris.hejblum@gmail.com>
2025-04-08 11:55:35 +0200

    updated python dependency management - still issues with pandas

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,12 @@
 .onLoad <- function(libname, pkgname) {
-  reticulate::configure_environment(pkgname)
+  reticulate::py_require("numpy")
+  reticulate::py_require("scikit-learn")
+  reticulate::py_require("scipy")
+  reticulate::py_require("pandas")
+  reticulate::py_require("rpy2")
+  reticulate::py_require("matplotlib")
+  reticulate::py_require("lightgbm")
+  reticulate::py_require("seaborn")
+  reticulate::py_require("hyperopt")
+  reticulate::py_require("reservoirpy")
 }
